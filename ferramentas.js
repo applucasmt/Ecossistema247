0271d46ae06fe3eeb3af94c3f2e54db748852525
applucasmt <veogera1@gmail.com>
2026-08-10 09:37:29 -0400

    Add files via upload

---
+++ ferramentas.js
@@ -0,0 +1,44 @@
+const ferramentasGabinete = [
+    {
+        id: "inicio",
+        nome: "Início",
+        icone: "fas fa-home",
+        arquivo: "inicio.html",
+        novaFuncao: false,
+        descricao: "O seu ecossistema integrado de ferramentas."
+    },
+    {
+        id: "gerador",
+        nome: "Gerador de Matérias",
+        icone: "fas fa-image",
+        arquivo: "gerador.html",
+        novaFuncao: false,
+        descricao: "Crie artes padronizadas para redes sociais rapidamente. Formatos otimizados para Feed e Stories."
+    },
+    {
+        id: "transcritor",
+        nome: "Transcritor AI",
+        icone: "fas fa-microphone-alt",
+        arquivo: "transcritor.html",
+        novaFuncao: false, 
+        descricao: "Transcrição avançada de áudio e vídeo com mapeamento de contexto e separação de vozes."
+    },
+    {
+        // NOVO MÓDULO ADICIONADO AQUI
+        id: "roteiro",
+        nome: "Roteiro de Filmagens",
+        icone: "fas fa-video",
+        arquivo: "roteiro.html",
+        novaFuncao: true,
+        descricao: "Gerador inteligente de roteiros de captação com sugestões de enquadramento e referências visuais em IA."
+    },
+     {
+        // NOVO MÓDULO ADICIONADO AQUI
+        id: "calendario",
+        nome: "Calendário 247",
+        icone: "fas fa-video",
+        arquivo: "calendario.html",
+        novaFuncao: true,
+        descricao: "Calendário de publucações e orientações para campanha"
+    }
+];
